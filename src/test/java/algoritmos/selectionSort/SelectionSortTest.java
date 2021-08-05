3d1251bca981822abedc9ee2ceb4d3c00079f42a
lmgbsb <leandro.mesquita.galvao@gmail.com>
2021-08-05 15:40:44 -0300

    adicionei teste de tempo de execução

--- src/test/java/algoritmos/selectionSort/SelectionSortTest.java
+++ src/test/java/algoritmos/selectionSort/SelectionSortTest.java
@@ -1,12 +1,11 @@
 package algoritmos.selectionSort;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import algoritmos.instacart.Instacart;
 
 class SelectionSortTest {
 	
@@ -22,30 +21,50 @@ class SelectionSortTest {
 	@DisplayName("Primeiro teste")
 	public void primeiroTeste() {		
 		int[] statues = {6, 2, 3, 8};
+		long tempoInicial = System.currentTimeMillis();
 		assertEquals(3, selectionSort.makeArrayConsecutive2(statues));
+		long tempoFinal = System.currentTimeMillis();
+		double tempoExecucao = ((tempoFinal - tempoInicial) / 1000);
+		assertTrue(tempoExecucao < 3);//tempo de execucao deve ser inferior a 3 segundos
 	}
 	@Test
 	@DisplayName("Segundo teste")
 	public void segundoteste() {
 		int[] statues = {0, 3};
+		long tempoInicial = System.currentTimeMillis();
 		assertEquals(2, selectionSort.makeArrayConsecutive2(statues));
+		long tempoFinal = System.currentTimeMillis();
+		double tempoExecucao = ((tempoFinal - tempoInicial) / 1000);
+		assertTrue(tempoExecucao < 3);//tempo de execucao deve ser inferior a 3 segundos		
 	}
 	@Test
 	@DisplayName("Terceiro teste")
 	public void terceiroTeste() {
 		int[] statues = {5, 4, 6};
+		long tempoInicial = System.currentTimeMillis();
 		assertEquals(0, selectionSort.makeArrayConsecutive2(statues));
+		long tempoFinal = System.currentTimeMillis();
+		double tempoExecucao = ((tempoFinal - tempoInicial) / 1000);
+		assertTrue(tempoExecucao < 3);//tempo de execucao deve ser inferior a 3 segundos			
 	}
 	@Test
 	@DisplayName("Quarto teste")
 	public void quartoTeste() {
 		int[] statues = {6, 3};
+		long tempoInicial = System.currentTimeMillis();
 		assertEquals(2, selectionSort.makeArrayConsecutive2(statues));
+		long tempoFinal = System.currentTimeMillis();
+		double tempoExecucao = ((tempoFinal - tempoInicial) / 1000);
+		assertTrue(tempoExecucao < 3);//tempo de execucao deve ser inferior a 3 segundos			
 	}
 	@Test
 	@DisplayName("Quinto teste")
 	public void quintoTeste() {
 		int[] statues = {1};
+		long tempoInicial = System.currentTimeMillis();
 		assertEquals(0, selectionSort.makeArrayConsecutive2(statues));
+		long tempoFinal = System.currentTimeMillis();
+		double tempoExecucao = ((tempoFinal - tempoInicial) / 1000);
+		assertTrue(tempoExecucao < 3);//tempo de execucao deve ser inferior a 3 segundos			
 	}
 }
